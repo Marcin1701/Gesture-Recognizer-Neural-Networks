611c15e2a21b30358edc47011155d06758af7104
Marcin <marcin.rogoz@outlook.ocm>
2021-08-01 12:05:49 +0200

    extend dataset

--- ModelTrainer.py
+++ ModelTrainer.py
@@ -15,11 +15,21 @@ ops.reset_default_graph()
 def create_output_vectors():
     output_vectors = []
     for i in range(0, 1000):
-        output_vectors.append([1, 0, 0])
+        output_vectors.append([1, 0, 0, 0, 0, 0, 0, 0])
     for i in range(0, 1000):
-        output_vectors.append([0, 1, 0])
+        output_vectors.append([0, 1, 0, 0, 0, 0, 0, 0])
     for i in range(0, 1000):
-        output_vectors.append([0, 0, 1])
+        output_vectors.append([0, 0, 1, 0, 0, 0, 0, 0])
+    for i in range(0, 1000):
+        output_vectors.append([0, 0, 0, 1, 0, 0, 0, 0])
+    for i in range(0, 1000):
+        output_vectors.append([0, 0, 0, 0, 1, 0, 0, 0])
+    for i in range(0, 1000):
+        output_vectors.append([0, 0, 0, 0, 0, 1, 0, 0])
+    for i in range(0, 1000):
+        output_vectors.append([0, 0, 0, 0, 0, 0, 1, 0])
+    for i in range(0, 1000):
+        output_vectors.append([0, 0, 0, 0, 0, 0, 0, 1])
     return output_vectors
 
 
@@ -41,6 +51,26 @@ def load_images():
         image = cv2.imread('Dataset/FistImages/fist_' + str(i) + '.png')
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         loaded_images.append(gray_image.reshape(89, 100, 1))
+    for i in range(0, 1000):
+        image = cv2.imread('Dataset/NoneImages/' + str(i) + '.png')
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        loaded_images.append(gray_image.reshape(256, 256, 1))
+    for i in range(1, 1001):
+        image = cv2.imread('Dataset/OkImages/image (' + str(i) + ').png')
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        loaded_images.append(gray_image.reshape(256, 256, 1))
+    for i in range(0, 1000):
+        image = cv2.imread('Dataset/PeaceImages/' + str(i) + '.png')
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        loaded_images.append(gray_image.reshape(256, 256, 1))
+    for i in range(0, 1000):
+        image = cv2.imread('Dataset/StraightImages/' + str(i) + '.png')
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        loaded_images.append(gray_image.reshape(256, 256, 1))
+    for i in range(0, 1000):
+        image = cv2.imread('Dataset/ThumbsImages/' + str(i) + '.png')
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        loaded_images.append(gray_image.reshape(256, 256, 1))
     return loaded_images
 
 
@@ -60,17 +90,47 @@ def load_test_images():
         image = cv2.imread('Dataset/FistTest/fist_' + str(i) + '.png')
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         test_images.append(gray_image.reshape(89, 100, 1))  # 1 - deklaruje ilość barw
+    for i in range(0, 100):
+        image = cv2.imread('Dataset/NoneImages/' + str(i) + '.png')
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        test_images.append(gray_image.reshape(256, 256, 1))
+    for i in range(0, 100):
+        image = cv2.imread('Dataset/OkTest/' + str(i) + '.png')
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        test_images.append(gray_image.reshape(256, 256, 1))
+    for i in range(0, 100):
+        image = cv2.imread('Dataset/PeaceTest/' + str(i) + '.png')
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        test_images.append(gray_image.reshape(256, 256, 1))
+    for i in range(0, 100):
+        image = cv2.imread('Dataset/StraightTest/' + str(i) + '.png')
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        test_images.append(gray_image.reshape(256, 256, 1))
+    for i in range(0, 100):
+        image = cv2.imread('Dataset/ThumbsTest/' + str(i) + '.png')
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        test_images.append(gray_image.reshape(256, 256, 1))
     return test_images
 
 
 def init_test_labels():
     test_labels = []
     for i in range(0, 100):
-        test_labels.append([1, 0, 0])
+        test_labels.append([1, 0, 0, 0, 0, 0, 0, 0])
     for i in range(0, 100):
-        test_labels.append([0, 1, 0])
+        test_labels.append([0, 1, 0, 0, 0, 0, 0, 0])
     for i in range(0, 100):
-        test_labels.append([0, 0, 1])
+        test_labels.append([0, 0, 1, 0, 0, 0, 0, 0])
+    for i in range(0, 100):
+        test_labels.append([0, 0, 0, 1, 0, 0, 0, 0])
+    for i in range(0, 100):
+        test_labels.append([0, 1, 0, 0, 1, 0, 0, 0])
+    for i in range(0, 100):
+        test_labels.append([0, 0, 0, 0, 0, 1, 0, 0])
+    for i in range(0, 100):
+        test_labels.append([0, 0, 0, 0, 0, 0, 1, 0])
+    for i in range(0, 100):
+        test_labels.append([0, 0, 0, 0, 0, 0, 0, 1])
     return test_labels
 
 
@@ -114,7 +174,7 @@ class ModelTrainer:
         self.conv_net = dropout(self.conv_net, 0.75)
         # 64 -> 3
         # 3 neurons on the output
-        self.conv_net = fully_connected(self.conv_net, 3, activation='softmax')
+        self.conv_net = fully_connected(self.conv_net, 8, activation='softmax')
 
         self.conv_net = regression(self.conv_net, optimizer='adam', learning_rate=0.001,
                                    loss='categorical_crossentropy',
