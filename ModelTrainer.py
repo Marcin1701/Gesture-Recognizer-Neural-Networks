472b2eb32022e8e4ef38fd959390df7555ece456
Marcin <marcin.rogoz@outlook.ocm>
2021-08-01 13:36:05 +0200

    extend dataset

--- ModelTrainer.py
+++ ModelTrainer.py
@@ -54,23 +54,23 @@ def load_images():
     for i in range(0, 1000):
         image = cv2.imread('Dataset/NoneImages/' + str(i) + '.png')
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        loaded_images.append(gray_image.reshape(256, 256, 1))
+        loaded_images.append(gray_image.reshape(89, 100, 1))
     for i in range(1, 1001):
         image = cv2.imread('Dataset/OkImages/image (' + str(i) + ').png')
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        loaded_images.append(gray_image.reshape(256, 256, 1))
+        loaded_images.append(gray_image.reshape(89, 100, 1))
     for i in range(0, 1000):
         image = cv2.imread('Dataset/PeaceImages/' + str(i) + '.png')
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        loaded_images.append(gray_image.reshape(256, 256, 1))
+        loaded_images.append(gray_image.reshape(89, 100, 1))
     for i in range(0, 1000):
         image = cv2.imread('Dataset/StraightImages/' + str(i) + '.png')
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        loaded_images.append(gray_image.reshape(256, 256, 1))
+        loaded_images.append(gray_image.reshape(89, 100, 1))
     for i in range(0, 1000):
         image = cv2.imread('Dataset/ThumbsImages/' + str(i) + '.png')
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        loaded_images.append(gray_image.reshape(256, 256, 1))
+        loaded_images.append(gray_image.reshape(89, 100, 1))
     return loaded_images
 
 
@@ -93,23 +93,23 @@ def load_test_images():
     for i in range(0, 100):
         image = cv2.imread('Dataset/NoneImages/' + str(i) + '.png')
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        test_images.append(gray_image.reshape(256, 256, 1))
+        test_images.append(gray_image.reshape(89, 100, 1))
     for i in range(0, 100):
         image = cv2.imread('Dataset/OkTest/' + str(i) + '.png')
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        test_images.append(gray_image.reshape(256, 256, 1))
+        test_images.append(gray_image.reshape(89, 100, 1))
     for i in range(0, 100):
         image = cv2.imread('Dataset/PeaceTest/' + str(i) + '.png')
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        test_images.append(gray_image.reshape(256, 256, 1))
+        test_images.append(gray_image.reshape(89, 100, 1))
     for i in range(0, 100):
         image = cv2.imread('Dataset/StraightTest/' + str(i) + '.png')
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        test_images.append(gray_image.reshape(256, 256, 1))
+        test_images.append(gray_image.reshape(89, 100, 1))
     for i in range(0, 100):
         image = cv2.imread('Dataset/ThumbsTest/' + str(i) + '.png')
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        test_images.append(gray_image.reshape(256, 256, 1))
+        test_images.append(gray_image.reshape(89, 100, 1))
     return test_images
 
 
@@ -138,7 +138,7 @@ class ModelTrainer:
 
     def train_network(self):
         # Train network,
-        self.model.fit(self.loadedImages, self.outputVectors, n_epoch=1,
+        self.model.fit(self.loadedImages, self.outputVectors, n_epoch=5,
                        validation_set=(
                            load_test_images(),
                            init_test_labels()
@@ -155,17 +155,17 @@ class ModelTrainer:
         self.conv_net = conv_2d(self.conv_net, 64, 2, activation='relu')
         self.conv_net = max_pool_2d(self.conv_net, 2)
         # 128 -> 256
-        # self.conv_net = conv_2d(self.conv_net, 128, 2, activation='relu')
-        # self.conv_net = max_pool_2d(self.conv_net, 2)
-        # # 256 -> 256
-        # self.conv_net = conv_2d(self.conv_net, 256, 2, activation='relu')
-        # self.conv_net = max_pool_2d(self.conv_net, 2)
-        # # 256 -> 128
-        # self.conv_net = conv_2d(self.conv_net, 256, 2, activation='relu')
-        # self.conv_net = max_pool_2d(self.conv_net, 2)
-        # # 128 -> 64
-        # self.conv_net = conv_2d(self.conv_net, 128, 2, activation='relu')
-        # self.conv_net = max_pool_2d(self.conv_net, 2)
+        self.conv_net = conv_2d(self.conv_net, 128, 2, activation='relu')
+        self.conv_net = max_pool_2d(self.conv_net, 2)
+        # 256 -> 256
+        self.conv_net = conv_2d(self.conv_net, 256, 2, activation='relu')
+        self.conv_net = max_pool_2d(self.conv_net, 2)
+        # 256 -> 128
+        self.conv_net = conv_2d(self.conv_net, 256, 2, activation='relu')
+        self.conv_net = max_pool_2d(self.conv_net, 2)
+        # 128 -> 64
+        self.conv_net = conv_2d(self.conv_net, 128, 2, activation='relu')
+        self.conv_net = max_pool_2d(self.conv_net, 2)
         # 64
         self.conv_net = conv_2d(self.conv_net, 64, 2, activation='relu')
         self.conv_net = max_pool_2d(self.conv_net, 2)
