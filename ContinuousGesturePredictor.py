6d1da9ee7b801b68560dae25bf0a6ea3f315d67e
Marcin Rogoż <marcin.rogoz@outlook.com>
2021-09-05 21:09:47 +0200

    delete some code

--- ContinuousGesturePredictor.py
+++ ContinuousGesturePredictor.py
@@ -44,18 +44,14 @@ def resizeImage(imageName):
 
 def run_avg(image, aWeight):
     global bg
-    # initialize the background
     if bg is None:
         bg = image.copy().astype("float")
         return
-
-    # compute weighted average, accumulate it and update the background
     cv2.accumulateWeighted(image, bg, aWeight)
 
 
 def segment(image, threshold=25):
     global bg
-    # find the absolute difference between background and current frame
     diff = cv2.absdiff(bg.astype("uint8"), image)
     thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
     (cnts, _) = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL,
@@ -63,7 +59,6 @@ def segment(image, threshold=25):
     if len(cnts) == 0:
         return
     else:
-        # based on contour area, get the maximum contour which is the hand
         segmented = max(cnts, key=cv2.contourArea)
         return thresholded, segmented
 
